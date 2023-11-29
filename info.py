9ed6443450d9312dab047b999cfb55cfee119f06
MahirModan <107642456+MahirModan@users.noreply.github.com>
2023-11-29 10:43:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ auth_grp = environ.get('AUTH_GROUP', '')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://fakeone:fakeone@cluster0.hkpcv31.mongodb.net/fakeone?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:net")
 DATABASE_NAME = environ.get('DATABASE_NAME', "fakeone")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
