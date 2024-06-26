a3e072bd9333c41798eb2f582d2779f75aa6dd7b
MahirModan <107642456+MahirModan@users.noreply.github.com>
2024-06-26 10:04:28 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('Hᴇʟᴘ', url=f"https://t.me/CinemaChatRoom"),
-            InlineKeyboardButton('Tᴜᴛᴏʀɪᴀʟ', url='https://t.me/CinemaRoom')
+            InlineKeyboardButton('Hᴇʟᴘ', url=f"https://t.me/ProPunjabiFilms"),
+            InlineKeyboardButton('Tᴜᴛᴏʀɪᴀʟ', url='https://t.me/ProPunjabiFilms')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
